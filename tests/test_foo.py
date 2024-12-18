e490f530f9ac1282fcc94cdd9767a6fcab939849
andrewtarzia <andrew.tarzia@gmail.com>
2024-12-18 20:57:21 +0100

    Reorganise folders.

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,5 +1,5 @@
-import millie
+import cage_construct
 
 
 def test_foo() -> None:
-    assert millie.foo(12) == "Hello, World! Also: 12"
+    assert cage_construct.foo(12) == "Hello, World! Also: 12"
